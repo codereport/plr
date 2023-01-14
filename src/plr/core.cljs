00b6785cc01071cd5d57da55bb747502b48a08bb
Conor Hoekstra <codereport@outlook.com>
2023-01-14 09:16:40 -0500

    Move logo-map to separate file

--- src/plr/core.cljs
+++ src/plr/core.cljs
@@ -1,62 +1,13 @@
-(ns demo.core
+(ns plr.core
   (:require
    [reagent.dom :as rdom]
    [reagent.core :as r]
    [clojure.string :as str]
+   [plr.imgs :as imgs]
    ))
 
 (defonce state (r/atom {:top-padding "100px"
                         :results-table [:tr]}))
-
-(def logo-map
-  {"APL"        "apl_logo.png"
-   "Assembly"   "assembly_logo.png"
-   "Shell"      "bash_logo.png"
-   "BQN"        "bqn_logo.svg"
-   "C"          "c_logo.png"
-   "C++"        "cpp_logo.png"
-   "C#"         "csharp_logo.png"
-   "Clojure"    "clojure_logo.png"
-   "Crystal"    "crystal_logo.svg"
-   "CSS"        "css_logo.png"
-   "CUDA"       "thrust_logo.jfif"
-   "D"          "d_logo.png"
-   "Dart"       "dart_logo.png"
-   "Elixir"     "elixir_logo.png"
-   "Elm"        "elm_logo.png"
-   "Erlang"     "erlang_logo.png"
-   "F#"         "fsharp_logo.png"
-   "Fortran"    "fortran_logo.png"
-   "Go"         "go_logo.png"
-   "Groovy"     "groovy_logo.jpeg"
-   "Haskell"    "haskell_logo.svg"
-   "HTML"       "html_logo.svg"
-   "J"          "j_logo.png"
-   "Java"       "java_logo.png"
-   "JavaScript" "javascript_logo.png"
-   "Julia"      "julia_logo.svg"
-   "Kotlin"     "kotlin_logo.png"
-   "LISP"       "LISP_logo.png"
-   "Lua"        "lua_logo.png"
-   "MATLAB"     "matlab_logo.png"
-   "Nim"        "nim_logo.png"
-   "Objective-C""objc_logo.png"
-   "OCaml"      "ocaml_logo.jpg"
-   "Pharo"      "pharo_logo.png"
-   "PHP"        "php_logo.png"
-   "PowerShell" "powershell_logo.png"
-   "Python"     "python_logo.png"
-   "q"          "kx-logo.png"
-   "R"          "r_logo.png"
-   "Racket"     "racket_logo.png"
-   "Ruby"       "ruby_logo.png"
-   "Rust"       "rust_logo.png"
-   "SAS"        "sas_logo.png"
-   "Scala"      "scala_logo2.png"
-   "SQL"        "sql_logo.png"
-   "Swift"      "swift_logo.png"
-   "TypeScript" "ts.png"
-   "Zig"        "zig_logo.svg"})
 
 (def octoverse ["JavaScript" "Python" "Java" "TypeScript" "C#" "C++" "PHP" "Shell" "C" "Ruby"])
 (def ieee ["Python" "C" "C++" "C#" "Java" "SQL" "JavaScript" "R" "HTML" "TypeScript" "Go" "PHP" "Shell" "Ruby" "Scala" "MATLAB" "SAS" "Assembly" "Kotlin" "Rust"])
@@ -72,7 +23,6 @@
              [166 "Ada"]
              [261 "Visual Basic"]])
 
-
 (def odd ["Markdown" "CSS" "HTML" "SAS" "Jupyter Notebook" "Vue" "JSON" "Dockerfile" "SQL"])
 
 (defn avg [coll] (/ (reduce + coll) (count coll)))
@@ -82,7 +32,7 @@
 (defn generate-row [rank [score n lang]]
   [:tr
    [:td {:style {:padding "12px 30px"}} (str (+ rank 1))]
-   [:td [:img {:src (str/join ["/media/" (get logo-map lang)]) :width "40px" :height "40px"}]]
+   [:td [:img {:src (str/join ["/media/" (get imgs/logo-map lang)]) :width "40px" :height "40px"}]]
    [:td {:style {:padding "12px 30px"}} (str lang)]
    [:td {:style {:padding "12px 30px"}} (format score)]
    [:td {:style {:padding "12px 30px"}} (format n)]])
