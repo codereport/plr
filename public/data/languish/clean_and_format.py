7d3f929909fffebad7692135526c2852bda43e5e
Conor Hoekstra <codereport@outlook.com>
2023-03-31 10:23:24 -0400

    REFACTOR #2

--- public/data/languish/clean_and_format.py
+++ public/data/languish/clean_and_format.py
@@ -2,12 +2,10 @@
 import sys
 from more_itertools import chunked
 
-o = open(sys.argv[1][:7] + ".txt", "w")
+o = open(sys.argv[1][:7] + "-test.txt", "w")
 
 with open(sys.argv[1]) as f:
-    lines = [','.join(c) for c in chunked(
-        [l.strip() for l in f.readlines() if l[0] not in ['-', '+']], 2)]
-    for line in lines:
-        o.write(line + "\n")
+    o.write('\n'.join(','.join(c) for c in chunked(
+        [l.strip() for l in f.readlines() if l[0] not in ['-', '+']], 2)))
 
 o.close()
