758638bb93186e216852845aee963267c62d56e3
Conor Hoekstra <codereport@outlook.com>
2023-06-02 10:36:55 -0400

    PYPL June Data

--- src/plr/helper.clj
+++ src/plr/helper.clj
@@ -5,8 +5,8 @@
 ; current
 (defmacro rm       [] (slurp "./public/data/rm/2022-06.txt"))
 (defmacro so       [] (slurp "./public/data/so/2022-06.txt"))
-(defmacro pypl     [] (slurp "./public/data/pypl/2023-05.txt"))
-(defmacro tiobe    [] (slurp "./public/data/tiobe/2023-04.txt"))
+(defmacro pypl     [] (slurp "./public/data/pypl/2023-06.txt"))
+(defmacro tiobe    [] (slurp "./public/data/tiobe/2023-05.txt"))
 (defmacro ieee     [] (slurp "./public/data/ieee/2022-08.txt"))
 (defmacro octo     [] (slurp "./public/data/octo/2022-11.txt"))
 (defmacro languish [] (slurp "./public/data/languish/2023-04.txt"))
@@ -14,8 +14,8 @@
 ; previous 3 month
 (defmacro prev-rm       [] (slurp "./public/data/rm/2022-06.txt"))
 (defmacro prev-so       [] (slurp "./public/data/so/2022-06.txt"))
-(defmacro prev-pypl     [] (slurp "./public/data/pypl/2023-02.txt"))
-(defmacro prev-tiobe    [] (slurp "./public/data/tiobe/2023-01.txt"))
+(defmacro prev-pypl     [] (slurp "./public/data/pypl/2023-03.txt"))
+(defmacro prev-tiobe    [] (slurp "./public/data/tiobe/2023-02.txt"))
 (defmacro prev-ieee     [] (slurp "./public/data/ieee/2022-08.txt"))
 (defmacro prev-octo     [] (slurp "./public/data/octo/2022-11.txt"))
 (defmacro prev-languish [] (slurp "./public/data/languish/2023-01.txt"))
