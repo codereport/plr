d5f1d0a4b6966d0478e9fc231c121d630fca8339
Conor Hoekstra <codereport@outlook.com>
2023-06-06 20:26:52 -0400

    update check_num:

--- public/data/check_numbering.py
+++ public/data/check_numbering.py
@@ -6,8 +6,8 @@ for subdir, dirs, files in os.walk('.'):
     for file in files:
         if len(file) == 11:
             file_path = os.path.join(subdir, file)
-            print(file_path)
             with open(file_path) as f:
                 numbers = [ int(line.split(',')[0]) for line in f.readlines() ]
-                print(("✅" if is_sorted(numbers) else "❌") + " - Numbers sorted")
-                print(("✅" if max(b - a for a, b in pairwise(numbers)) <= 1 else "❌") + " - Max Gap <= 1")
+                print(("✅" if is_sorted(numbers) else "❌ - Numbers sorted" + file_path), end = '')
+                print(("✅" if max(b - a for a, b in pairwise(numbers)) <= 1 else "❌ - Max Gap <= 1" + file_path), end = '')
+print()
