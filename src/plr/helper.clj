6f4ea6ffb37b27791062e8e1c58f9ae11ee13b12
Conor Hoekstra <codereport@outlook.com>
2023-07-10 09:32:04 -0400

    Add TIOBE 2023 July Data

--- src/plr/helper.clj
+++ src/plr/helper.clj
@@ -6,7 +6,7 @@
 (defmacro rm       [] (slurp "./public/data/rm/2023-01.txt")) ; released on 2023-05
 (defmacro so       [] (slurp "./public/data/so/2023-06.txt"))
 (defmacro pypl     [] (slurp "./public/data/pypl/2023-07.txt"))
-(defmacro tiobe    [] (slurp "./public/data/tiobe/2023-06.txt"))
+(defmacro tiobe    [] (slurp "./public/data/tiobe/2023-07.txt"))
 (defmacro ieee     [] (slurp "./public/data/ieee/2022-08.txt"))
 (defmacro octo     [] (slurp "./public/data/octo/2022-11.txt"))
 (defmacro languish [] (slurp "./public/data/languish/2023-07.txt"))
@@ -15,7 +15,7 @@
 (defmacro prev-rm       [] (slurp "./public/data/rm/2022-06.txt")) ; released on 2022-10
 (defmacro prev-so       [] (slurp "./public/data/so/2022-06.txt"))
 (defmacro prev-pypl     [] (slurp "./public/data/pypl/2023-04.txt"))
-(defmacro prev-tiobe    [] (slurp "./public/data/tiobe/2023-03.txt"))
+(defmacro prev-tiobe    [] (slurp "./public/data/tiobe/2023-04.txt"))
 (defmacro prev-ieee     [] (slurp "./public/data/ieee/2022-08.txt"))
 (defmacro prev-octo     [] (slurp "./public/data/octo/2022-11.txt"))
 (defmacro prev-languish [] (slurp "./public/data/languish/2023-04.txt"))
