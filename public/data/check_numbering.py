66ad54aa4b2f32b2096b8753b4dcb32eeef08a23
Conor Hoekstra <codereport@outlook.com>
2023-10-02 14:50:15 -0400

    :sparkles: add check for data EOF blank lines

--- public/data/check_numbering.py
+++ public/data/check_numbering.py
@@ -1,3 +1,4 @@
+#! /usr/bin/env python3
 
 import os
 from more_itertools import is_sorted, pairwise
@@ -10,4 +11,5 @@ for subdir, dirs, files in os.walk('.'):
                 numbers = [ int(line.split(',')[0]) for line in f.readlines() ]
                 print(("✅" if is_sorted(numbers) else "❌ - Numbers sorted" + file_path), end = '')
                 print(("✅" if max(b - a for a, b in pairwise(numbers)) <= 1 else "❌ - Max Gap <= 1" + file_path), end = '')
+            print(("✅" if not open(file_path).readlines()[-1].endswith('\n') else "❌ - Blank EOF Line" + file_path), end = '')
 print()
