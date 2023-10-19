02eb5e461abca1b8af1c3c7ad8685b158079c036
Ty <tytr@DESKTOP-L1J4QPI>
2023-10-19 19:02:55 -0400

    Adding options for 50 and 100 languages
    Adding options to pull 50 and 100 languages. Choosing 100 creates a visual artifact, so I'll tackle that next.

--- src/plr/core.cljs
+++ src/plr/core.cljs
@@ -150,7 +150,7 @@
               [:label styles/cb-font "Number of Languages: "]
               [:select {:value (@state :num-langs)
                         :on-change #(swap! state assoc :num-langs (-> % .-target .-value js/Number))}
-               [:option 10] [:option 20]]] [:br]
+               [:option 10] [:option 20] [:option 50] [:option 100]]] [:br]
              [:form {:style {:display "inline"}}
               [:label styles/cb-font "Months for Delta (Δ): "]
               [:select {:value (@state :delta)
