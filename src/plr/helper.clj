419f0c8c5cd056ae12166ad1f6ece6bde07a278d
Conor Hoekstra <codereport@outlook.com>
2023-12-21 13:39:44 -0500

    :sparkles: dec data

--- src/plr/helper.clj
+++ src/plr/helper.clj
@@ -5,8 +5,8 @@
 ; current
 (defmacro rm       [] (slurp "./public/data/rm/2023-01.txt")) ; released on 2023-05
 (defmacro so       [] (slurp "./public/data/so/2023-06.txt"))
-(defmacro pypl     [] (slurp "./public/data/pypl/2023-11.txt"))
-(defmacro tiobe    [] (slurp "./public/data/tiobe/2023-11.txt"))
+(defmacro pypl     [] (slurp "./public/data/pypl/2023-12.txt"))
+(defmacro tiobe    [] (slurp "./public/data/tiobe/2023-12.txt"))
 (defmacro ieee     [] (slurp "./public/data/ieee/2023-08.txt"))
 (defmacro octo     [] (slurp "./public/data/octo/2023-11.txt"))
 (defmacro languish [] (slurp "./public/data/languish/2023-10.txt"))
@@ -14,8 +14,8 @@
 ; previous 3 month
 (defmacro prev3-rm       [] (slurp "./public/data/rm/2022-06.txt")) ; released on 2022-10
 (defmacro prev3-so       [] (slurp "./public/data/so/2022-06.txt"))
-(defmacro prev3-pypl     [] (slurp "./public/data/pypl/2023-08.txt"))
-(defmacro prev3-tiobe    [] (slurp "./public/data/tiobe/2023-08.txt"))
+(defmacro prev3-pypl     [] (slurp "./public/data/pypl/2023-09.txt"))
+(defmacro prev3-tiobe    [] (slurp "./public/data/tiobe/2023-09.txt"))
 (defmacro prev3-ieee     [] (slurp "./public/data/ieee/2022-08.txt"))
 (defmacro prev3-octo     [] (slurp "./public/data/octo/2022-11.txt"))
 (defmacro prev3-languish [] (slurp "./public/data/languish/2023-07.txt"))
@@ -23,8 +23,8 @@
 ; previous 6 month
 (defmacro prev6-rm       [] (slurp "./public/data/rm/2022-06.txt")) ; released on 2022-10
 (defmacro prev6-so       [] (slurp "./public/data/so/2022-06.txt"))
-(defmacro prev6-pypl     [] (slurp "./public/data/pypl/2023-05.txt"))
-(defmacro prev6-tiobe    [] (slurp "./public/data/tiobe/2023-05.txt"))
+(defmacro prev6-pypl     [] (slurp "./public/data/pypl/2023-06.txt"))
+(defmacro prev6-tiobe    [] (slurp "./public/data/tiobe/2023-06.txt"))
 (defmacro prev6-ieee     [] (slurp "./public/data/ieee/2022-08.txt"))
 (defmacro prev6-octo     [] (slurp "./public/data/octo/2022-11.txt"))
 (defmacro prev6-languish [] (slurp "./public/data/languish/2023-04.txt"))
