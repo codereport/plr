d807268cc5e61e74b0e337b26495f39bee9ec835
Conor Hoekstra <codereport@outlook.com>
2024-09-16 19:19:37 -0400

    :sparkles: update redmonk and site

--- src/plr/helper.clj
+++ src/plr/helper.clj
@@ -3,7 +3,7 @@
 ; figure out how to parameterize on data/sites and info/last-update
 
 ; current
-(defmacro rm       [] (slurp "./public/data/rm/2023-01.txt")) ; released on 2023-05
+(defmacro rm       [] (slurp "./public/data/rm/2024-06.txt"))
 (defmacro so       [] (slurp "./public/data/so/2024-07.txt"))
 (defmacro pypl     [] (slurp "./public/data/pypl/2024-07.txt"))
 (defmacro tiobe    [] (slurp "./public/data/tiobe/2023-12.txt"))
@@ -12,7 +12,7 @@
 (defmacro languish [] (slurp "./public/data/languish/2024-07.txt"))
 
 ; previous 3 month
-(defmacro prev3-rm       [] (slurp "./public/data/rm/2022-06.txt")) ; released on 2022-10
+(defmacro prev3-rm       [] (slurp "./public/data/rm/2024-01.txt"))
 (defmacro prev3-so       [] (slurp "./public/data/so/2023-06.txt"))
 (defmacro prev3-pypl     [] (slurp "./public/data/pypl/2024-04.txt"))
 (defmacro prev3-tiobe    [] (slurp "./public/data/tiobe/2023-09.txt"))
@@ -21,7 +21,7 @@
 (defmacro prev3-languish [] (slurp "./public/data/languish/2024-04.txt"))
 
 ; previous 6 month
-(defmacro prev6-rm       [] (slurp "./public/data/rm/2022-06.txt")) ; released on 2022-10
+(defmacro prev6-rm       [] (slurp "./public/data/rm/2024-01.txt"))
 (defmacro prev6-so       [] (slurp "./public/data/so/2023-06.txt"))
 (defmacro prev6-pypl     [] (slurp "./public/data/pypl/2024-01.txt"))
 (defmacro prev6-tiobe    [] (slurp "./public/data/tiobe/2023-07.txt"))
@@ -30,7 +30,7 @@
 (defmacro prev6-languish [] (slurp "./public/data/languish/2024-01.txt"))
 
 ; previous 12 month
-(defmacro prev12-rm       [] (slurp "./public/data/rm/2022-06.txt")) ; released on 2022-10
+(defmacro prev12-rm       [] (slurp "./public/data/rm/2023-01.txt"))
 (defmacro prev12-so       [] (slurp "./public/data/so/2022-06.txt"))
 (defmacro prev12-pypl     [] (slurp "./public/data/pypl/2023-09.txt"))
 (defmacro prev12-tiobe    [] (slurp "./public/data/tiobe/2023-01.txt"))
